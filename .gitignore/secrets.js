ac6abe3370f59b45eb71e958f188c8482f11d1d8
Sarah Skinner <skinnersarah93@gmail.com>
2021-11-26 15:24:44 -0500

    fix api

--- .gitignore/secrets.js
+++
@@ -1,2 +0,0 @@
-let ipinfo_token = "124667c8994bc6";
-let weather_token = "e3e67c3cef974039f2ccd4fb5fea822e";
--- js/find_location.js
+++ js/find_location.js
@@ -1,4 +1,6 @@
-const this_secret = require(['.gitignore/secrets.js']);
+//these are api keys you can get for free If you want your own, they're free
+ipinfo_token = "124667c8994bc6";
+weather_token = "e3e67c3cef974039f2ccd4fb5fea822e";
 
 function default_bg(){
 	document.getElementById("greg").style.backgroundImage = "url('css/bg9.jpg')";
