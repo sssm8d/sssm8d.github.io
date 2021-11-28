c9c992769625c8b78a750a46750250605ff4fcda
Sarah Skinner <skinnersarah93@gmail.com>
2021-11-27 23:19:03 -0500

    Added moon and ominous desc

--- js/find_location.js
+++ js/find_location.js
@@ -1,15 +1,26 @@
 //these are api keys you can get for free If you want your own, they're free
 ipinfo_token = "124667c8994bc6";
 weather_token = "e3e67c3cef974039f2ccd4fb5fea822e";
+var hemisphere = "None";
 
 function default_bg(){
 	document.getElementById("greg").style.backgroundImage = "url('css/bg9.jpg')";
 	
 	document.getElementById("intro").innerHTML = "That's me. This is what I'm doing right now.";
 	document.getElementById("github").innerHTML = "See my contributions to collaborations as well as individual projects.";
-	document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
+	// document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
 	document.getElementById("linkedin").innerHTML = "Look at my resume.";
 	document.getElementById("insta").innerHTML = "Look at pictures of my cat Rockstar.";
+	
+	//generate a random number
+	let emojis = [9749,9996,9997,9999,10002,10024,127183,127378,127384,127744,127752,127756,127758,127794,127849,127918,128008,128049,128062,128126]
+	let emojis1 = [128128,128214,128218,128221,128225,128568,128640,128701,129496,129497,129498	]
+	emojis = emojis.concat(emojis1)
+	var randomNumber = Math.floor(Math.random() * emojis.length);
+		
+	//put the emoji id number into the format the html page will accept (&#000000)
+	const phase = getLunarPhaseNorthern(date);
+	document.getElementById("tim").innerHTML = ( phase );
 }
 
 fetch('https://api.ipify.org/?format=json')
@@ -40,13 +51,45 @@ fetch('https://api.ipify.org/?format=json')
 				}
 				document.getElementById("greg").style.backgroundImage = "url('js/weather_bg/"+weather_data.weather[0].main+this_time+".jpg')";
 				
+				var latangle = (weather_data.coord.lat-40.5);
+				if (latangle<-180){
+					latangle = 360+latangle;
+				}
+				var latlength = latangle/180.0*3958.8*Math.PI;
+				var lonangle = (weather_data.coord.lon+80.0);
+				if (lonangle>180){
+					lonangle = 360-lonangle;
+				}
+				var lonlength = lonangle/180.0*3958.8*Math.PI;
+				var distance = Math.sqrt( latlength*latlength+lonlength*lonlength);
+				
+				var temp_message = "";
+				if (weather_data.main.temp < 283.15){
+					temp_message = "Keep warm inside while you checkout my resume."
+				}else if ( weather_data.main.temp > 305.372 ){
+					temp_message = "Stay cool inside while you checkout my resume."
+				}
+				
+				
+				//fill out words
 				document.getElementById("intro").innerHTML = "That's me. This is what I'm doing right now.";
 				document.getElementById("github").innerHTML = "See my contributions to collaborations as well as individual projects.";
-				document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
-				document.getElementById("linkedin").innerHTML = "Look at my resume.";
+				if (temp_message == ""){
+					document.getElementById("linkedin").innerHTML = "Look at my resume.";
+				}else{
+					document.getElementById("linkedin").innerHTML = temp_message;
+				}
+				document.getElementById("upwork").innerHTML = "Just because we're "+distance.toFixed(2)+" miles apart, doesn't mean we can't work together!";
 				document.getElementById("insta").innerHTML = "Look at pictures of my cat Rockstar.";
 				
-				
+				//add mon phase at bottom
+				if (weather_data.coord.lat > 0.0){
+					const phase = getLunarPhaseNorthern(date);
+					document.getElementById("tim").innerHTML = ( phase );
+				}else{
+					const phase = getLunarPhaseSouthern(date);
+					document.getElementById("tim").innerHTML = ( phase );
+				}
 				
 			})
 			.catch(function(error) {
---
+++ js/lunar_phase.js
@@ -0,0 +1,77 @@
+//stolen from https://jasonsturges.medium.com/moons-lunar-phase-in-javascript-a5219acbfe6e
+
+const getJulianDate = (date = new Date()) => {
+  const time = date.getTime();
+  const tzoffset = date.getTimezoneOffset()
+  
+  return (time / 86400000) - (tzoffset / 1440) + 2440587.5;
+}
+
+const LUNAR_MONTH = 29.530588853;
+const getLunarAge = (date = new Date()) => {
+  const percent = getLunarAgePercent(date);
+  const age = percent * LUNAR_MONTH;
+  return age;
+}
+const getLunarAgePercent = (date = new Date()) => {
+  return normalize((getJulianDate(date) - 2451550.1) / LUNAR_MONTH);
+}
+const normalize = value => {
+  value = value - Math.floor(value);
+  if (value < 0)
+    value = value + 1
+  return value;
+}
+
+const getLunarPhaseNorthern = (date = new Date()) => {
+  const age = getLunarAge(date);
+  if (age < 1.84566)
+    return "&#127761;";
+  else if (age < 5.53699)
+    return "&#127762;";
+  else if (age < 9.22831)
+    return "&#127763;";
+  else if (age < 12.91963)
+    return "&#127764;";
+  else if (age < 16.61096)
+    return "&#127765;";
+  else if (age < 20.30228)
+    return "&#127766;";
+  else if (age < 23.99361)
+    return "&#127767;";
+  else if (age < 27.68493)
+    return "&#127768;";
+  return "&#127770;";
+}
+
+const getLunarPhaseSouthern = (date = new Date()) => {
+  const age = getLunarAge(date);
+  if (age < 1.84566)
+    return "&#127761;";
+  else if (age < 5.53699)
+    return "&#127768;";
+  else if (age < 9.22831)
+    return "&#127767;";
+  else if (age < 12.91963)
+    return "&#127766;";
+  else if (age < 16.61096)
+    return "&#127765;";
+  else if (age < 20.30228)
+    return "&#127764;";
+  else if (age < 23.99361)
+    return "&#127763;";
+  else if (age < 27.68493)
+    return "&#127762;";
+  return "&#127770;";
+}
+
+const isWaxing = (date = new Date()) => {
+  const age = getLunarAge(date);
+  return age <= 14.765;
+}
+const isWaning = (date = new Date()) => {
+  const age = getLunarAge(date);
+  return age > 14.765;
+}
+
+const date = new Date();
