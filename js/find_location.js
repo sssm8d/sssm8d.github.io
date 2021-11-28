9e5332fdd44a862ce15aab60ba2cd3bddd96adde
Sarah Skinner <skinnersarah93@gmail.com>
2021-11-27 19:28:57 -0500

    Added night/day pictures for background

--- js/find_location.js
+++ js/find_location.js
@@ -4,6 +4,12 @@ weather_token = "e3e67c3cef974039f2ccd4fb5fea822e";
 
 function default_bg(){
 	document.getElementById("greg").style.backgroundImage = "url('css/bg9.jpg')";
+	
+	document.getElementById("intro").innerHTML = "That's me. This is what I'm doing right now.";
+	document.getElementById("github").innerHTML = "See my contributions to collaborations as well as individual projects.";
+	document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
+	document.getElementById("linkedin").innerHTML = "Look at my resume.";
+	document.getElementById("insta").innerHTML = "Look at pictures of my cat Rockstar.";
 }
 
 fetch('https://api.ipify.org/?format=json')
@@ -19,7 +25,28 @@ fetch('https://api.ipify.org/?format=json')
 			fetch(url2)
 			.then(response => response.json())
 			.then(function(weather_data){
-				document.getElementById("greg").style.backgroundImage = "url('js/weather_bg/"+weather_data.weather[0].main.toString()+".jpg')";
+				this_time = "Day";
+				
+				var now = new Date().getTime() / 1000;
+				if ( now>weather_data.sys.sunrise && now<weather_data.sys.sunset ){
+					this_time = "Day";
+					if (weather_data.weather[0].main=="Snow" || weather_data.weather[0].main=="Clear" || 
+					weather_data.weather[0].main=="Smoke" || weather_data.weather[0].main=="Clouds"){
+						document.getElementById("backtotop").style.color = "black";
+					}
+				}
+				else{
+					this_time = "Night";
+				}
+				document.getElementById("greg").style.backgroundImage = "url('js/weather_bg/"+weather_data.weather[0].main+this_time+".jpg')";
+				
+				document.getElementById("intro").innerHTML = "That's me. This is what I'm doing right now.";
+				document.getElementById("github").innerHTML = "See my contributions to collaborations as well as individual projects.";
+				document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
+				document.getElementById("linkedin").innerHTML = "Look at my resume.";
+				document.getElementById("insta").innerHTML = "Look at pictures of my cat Rockstar.";
+				
+				
 				
 			})
 			.catch(function(error) {
@@ -40,13 +67,6 @@ fetch('https://api.ipify.org/?format=json')
 	console.log(error);
 	default_bg();
   });
-
-
-document.getElementById("intro").innerHTML = "That's me. This is what I'm doing right now.";
-document.getElementById("github").innerHTML = "See my contributions to collaborations as well as individual projects.";
-document.getElementById("upwork").innerHTML = "Hire me as a freelancer.";
-document.getElementById("linkedin").innerHTML = "Look at my resume.";
-document.getElementById("insta").innerHTML = "Look at pictures of my cat Rockstar.";
 
 //get use ipinfo
 //get weather based off of zip code
